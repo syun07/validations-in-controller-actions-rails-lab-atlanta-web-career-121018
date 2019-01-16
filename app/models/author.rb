1f2bcdcca7f732c51ebc0e832920c2e781c5c3af
Sarah <sarahyun7@gmail.com>
2019-01-16 09:58:36 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  #name cannot be blank
+  validates :name, length: { minimum: 1 }
+
+  #email is unique
+  validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+  #title cannot be blank
+  validates :title, length: { minimum: 1 }
+
+  #category is either fiction/non-fiction
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction )}
+
+  #content must be at least 100 characters
+  validates :content, length: { minimum: 100 }
 end
